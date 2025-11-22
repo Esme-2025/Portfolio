b7058dd4fc7617d77ae189303cc1429af02616d1
Esme-2025 <esmeraldammelgarf4@gmail.com>
2025-11-21 19:03:14 -0600

    Creacion de los Objetos, instalacion de EntityFrameworks

---
+++ Models/StudentProfile.cs
@@ -0,0 +1,22 @@
+﻿using Portfolio.Models;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+
+public class StudentProfile
+{
+    public int Id { get; set; }
+     public string FullName { get; set; }
+    public string Title { get; set; }
+    public string Summary { get; set; }
+    public string Email { get; set; }
+    public string Location { get; set; }
+// imagen o avatar 
+    public string ProfileImageUrl {get; set;}
+
+    public virtual ICollection<Education> Education { get; set; }
+    public virtual ICollection<Experience> Experience { get; set; }
+    public virtual ICollection<Skills> Skills { get; set; }
+    public virtual ICollection<Projects> Projects { get; set; }
+    public virtual ICollection<SocialLinks> SocialLinks { get; set; }
+
+}
